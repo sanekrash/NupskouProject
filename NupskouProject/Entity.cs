e50a335ce123e6fb41c2beb748325067cb2312b7
sanekrash <sanekrash.a@yandex.ru>
2018-07-20 17:51:20 +0300

    nupskou 0.0.(0)31

---
+++ NupskouProject/Entity.cs
@@ -0,0 +1,28 @@
+﻿namespace NupskouProject
+{
+    abstract class Entity
+    {
+        public void Render()
+        {
+    
+        }
+
+        public void Update()
+        {
+            
+        }
+    }
+
+    class Bullet : Entity
+    {
+        public void Render()
+        {
+    
+        }
+
+        public void Update()
+        {
+            
+        }
+    }
+}
