28719fae6a037f97bb2515b4d7336f83d61786e7
sanekrash <sanekrash.a@yandex.ru>
2018-07-25 20:40:58 +0300

    changing in spellcard

--- NupskouProject/Rashka/ShootTheLalkaSpawner.cs
+++ NupskouProject/Rashka/ShootTheLalkaSpawner.cs
@@ -1,7 +1,10 @@
-﻿using Microsoft.Xna.Framework;
+﻿using System.Diagnostics;
+using System.Threading;
+using Microsoft.Xna.Framework;
 using NupskouProject.Core;
 using NupskouProject.Entities;
 using NupskouProject.Math;
+using NupskouProject.Raden.Utils;
 using NupskouProject.Rashka.Bullets;
 using NupskouProject.Utils;
 
@@ -12,6 +15,7 @@ namespace NupskouProject.Rashka {
 
         private XY  _p;
         private int _w;
+        private byte shotgun;
         
 
 
@@ -25,7 +29,7 @@ namespace NupskouProject.Rashka {
             if (t % 15 == 0) {
                 SpawnDanmaku ();
             }
-            if (t % 120 == 0) {
+            if (t % 120 == 0 && t != 0) {
                 world.Spawn (
                     new MarkRayTrigger (
                         new XY (The.Player.Position.X, 0),
@@ -43,10 +47,15 @@ namespace NupskouProject.Rashka {
                     )
                 );
             }
+
+            if (t % 360 == 0 && t != 0)
+            {
+                Shotgun ();
+            }
         }
 
 
-        private static void SpawnDanmaku () {
+        private void SpawnDanmaku () {
             for (int i = 0; i < 4; i++) {
                 The.World.Spawn (
                     new Bullet (
@@ -57,6 +66,19 @@ namespace NupskouProject.Rashka {
                 );
             }
         }
+        
+
+        private void Shotgun () {
+            for (int i = 0; i < 6; i++)
+            {
+                The.World.Spawn(new MarkRayTrigger(_p,
+                    XY.DirectionAngle(_p, The.Player.Position) + The.Random.Float(-Mathf.PI / 4, Mathf.PI / 4), 30,
+                    Color.Red));
+
+            }
+        }
+        
+
 
     }
 
