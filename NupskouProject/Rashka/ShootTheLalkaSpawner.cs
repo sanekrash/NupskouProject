bcd19d89d8068845461f9734c7a9418f6e4a29b9
sanekrash <sanekrash.a@yandex.ru>
2018-07-26 14:47:51 +0300

    add more FUUN

--- NupskouProject/Rashka/ShootTheLalkaSpawner.cs
+++ NupskouProject/Rashka/ShootTheLalkaSpawner.cs
@@ -14,6 +14,12 @@ namespace NupskouProject.Rashka {
     public class ShootTheLalkaSpawner : Entity {
 
         private XY  _p;
+        private int _w = The.Difficulty.Choose (15, 30, 45, 60);
+        private int _danmakuInterval = The.Difficulty.Choose (55, 35, 25, 15);
+        private int _danmakuInterval1 = The.Difficulty.Choose (24, 24, 12, 6);
+
+        private float _cone = The.Difficulty.Choose (Mathf.PI/24 , Mathf.PI/24, Mathf.PI/12, Mathf.PI/12);
+
         
 
 
@@ -24,15 +30,15 @@ namespace NupskouProject.Rashka {
 
         public override void Update (int t) {
             var world = The.World;
-            if (t % 15 == 0) {
+            if (t % _danmakuInterval == 0) {
                 SpawnDanmaku ();
             }
-            if (t % 120 == 0 && t != 0) {
+            if (t % 180 == 0 && t != 0) {
                 world.Spawn (
                     new MarkRayTrigger (
                         new XY (The.Player.Position.X, 0),
                         Mathf.PI / 2,
-                        60,
+                        _w,
                         Color.LimeGreen
                     )
                 );
@@ -40,16 +46,16 @@ namespace NupskouProject.Rashka {
                     new MarkRayTrigger (
                         new XY (0, The.Player.Position.Y),
                         0,
-                        60,
+                        _w,
                         Color.LimeGreen
                     )
                 );
             }
 
-            if (t % 360 < 60 && t % 10 == 0 && !(t <= 60))
+            if (t % 360 < 90 && t % _danmakuInterval1 == 0 && !(t <= 90))
             {
                 The.World.Spawn(new MarkRayTrigger(_p,
-                    XY.DirectionAngle(_p, The.Player.Position) + The.Random.Float(-Mathf.PI / 6, Mathf.PI / 6), 30,
+                    XY.DirectionAngle(_p, The.Player.Position) + The.Random.Float(-_cone, _cone), _w/2f,
                     Color.Red));
             }
         }
