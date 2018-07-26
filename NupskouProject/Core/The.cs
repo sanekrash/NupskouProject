3ac12970ff0f95f21cf849b0e24f36767fcd494d
sanekrash <sanekrash.a@yandex.ru>
2018-07-26 17:06:57 +0300

    Merge problems

--- NupskouProject/Core/The.cs
+++ NupskouProject/Core/The.cs
@@ -11,10 +11,8 @@ namespace NupskouProject.Core {
         public static Random      Random     = new Random ();
         public static Renderer    Renderer   = new Renderer ();
         public static Difficulty  Difficulty = Difficulty.Normal;
-<<<<<<< HEAD
 //        public static SpriteBatch SpriteBatch;
-=======
->>>>>>> f89f572a53c5b3c44d29c1927f60a66f32f904b5
+
         public static Player Player;
 
     }
