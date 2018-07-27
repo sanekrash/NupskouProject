ce99929935c186ecbeae3df6068ce1e89ca89f21
sanekrash <sanekrash.a@yandex.ru>
2018-07-27 12:15:49 +0300

    = )

--- NupskouProject/Rashka/SmileSpawner.cs
+++ NupskouProject/Rashka/SmileSpawner.cs
@@ -14,28 +14,34 @@ namespace NupskouProject.Rashka {
     public class SmileSpawner : Entity {
 
         public override void Update (int t) {
-            if (t % 15 == 0) {
+            if (t % 30 == 0) {
                 var box = World.Box;
                 SpawnSmile (
                     new XY (The.Random.Float (box.Left + 150, box.Right - 150), -100),
-                    6 * new XY (The.Random.SignedFloat () * Mathf.PI / 1.5f).Rotated90CCW ()
+                    6 * new XY (The.Random.SignedFloat () * Mathf.PI / 1.5f).Rotated90CCW (),
+                    Color.Red
+                    );
+                SpawnSmile (
+                    new XY (The.Random.Float (box.Left + 150, box.Right - 150), -100),
+                    6 * new XY (The.Random.SignedFloat () * Mathf.PI / 1.5f).Rotated90CCW (),
+                    Color.OrangeRed
                 );
             }
         }
 
 
-        private void SpawnSmile (XY p, XY v) {
+        private void SpawnSmile (XY p, XY v, Color color) {
             var w     = v.Normalized;
             var line  = Danmaku.Line (w, 0, 40, 5);
             var spray = Danmaku.Spray (90 * w, Mathf.PI / 3, 9);
 
             var world = The.World;
             foreach (var offset in line) {
-                world.Spawn (new VerticalBounceBullet (p + 20 * w.Rotated90CW () + offset, v, Color.Red));
-                world.Spawn (new VerticalBounceBullet (p - 20 * w.Rotated90CW () + offset, v, Color.Red));
+                world.Spawn (new VerticalBounceBullet (p + 20 * w.Rotated90CW () + offset, v, color));
+                world.Spawn (new VerticalBounceBullet (p - 20 * w.Rotated90CW () + offset, v, color));
             }
             foreach (var offset in spray) {
-                world.Spawn (new VerticalBounceBullet (p + offset, v, Color.Red));
+                world.Spawn (new VerticalBounceBullet (p + offset, v, color));
             }
         }
 
