db587ce451af19cea7e67821d7fba7a138c95162
sanekrash <sanekrash.a@yandex.ru>
2018-07-27 07:30:52 +0300

    = )

--- NupskouProject/Rashka/Bullets/VerticalBounceBullet.cs
+++ NupskouProject/Rashka/Bullets/VerticalBounceBullet.cs
@@ -15,6 +15,7 @@ namespace NupskouProject.Rashka.Bullets {
         private readonly Color _color;
 
         private XY _p;
+        private bool _a = false;
 
 
         public VerticalBounceBullet (XY p0, XY v, Color color) {
@@ -30,14 +31,26 @@ namespace NupskouProject.Rashka.Bullets {
             if (!Geom.CircleInVerticalBorders (new Circle (_p, 6), World.Box)) {
                 Explode ();
             }
+            _p = _p0 + t * _v;
+            if (!Geom.CircleOverBox (new Circle (_p, 6), World.Box)) {
+                Despawn ();                
+            }
+
+            _a = true;
 
         }
 
 
         private void Explode ()
         {
-            Despawn();
-            The.World.Spawn (new VerticalBounceBullet (_p+new XY(-_v.X,_v.Y), new XY(-_v.X,_v.Y), _color));
+            if (_a) {
+                Despawn();
+                The.World.Spawn(new VerticalBounceBullet(_p/* + new XY(-_v.X, _v.Y)*/, new XY(-_v.X, _v.Y), _color));
+            }
+            else
+            {
+                _a = true;
+            }
         }
 
 
--- NupskouProject/Rashka/SmileSpawner.cs
+++ NupskouProject/Rashka/SmileSpawner.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Xna.Framework;
+﻿using System;
+using System.Linq.Expressions;
+using Microsoft.Xna.Framework;
 using NupskouProject.Core;
 using NupskouProject.Entities;
 using NupskouProject.Math;
@@ -12,8 +14,10 @@ namespace NupskouProject.Rashka {
     public class SmileSpawner : Entity {
 
         public override void Update (int t) {
-            if (t % 30 == 0) {
-                SpawnSmile (World.BossPlace, 4 * new XY(The.Random.Angle ()));
+            if (t % 15 == 0)
+            {
+                SpawnSmile(new XY(The.Random.Float(World.Box.Left+150, World.Box.Right-150), 0),
+                    6*(new XY(Mathf.PI/2+The.Random.Float(-Mathf.PI/3,Mathf.PI/3))));
             }
         }
 
@@ -25,11 +29,11 @@ namespace NupskouProject.Rashka {
 
             var world = The.World;
             foreach (var offset in line) {
-                world.Spawn (new Bullet (p + 20 * w.Rotated90CW () + offset, v, Color.Red));
-                world.Spawn (new Bullet (p - 20 * w.Rotated90CW () + offset, v, Color.Red));
+                world.Spawn (new VerticalBounceBullet(p + 20 * w.Rotated90CW () + offset, v, Color.Red));
+                world.Spawn (new VerticalBounceBullet(p - 20 * w.Rotated90CW () + offset, v, Color.Red));
             }
             foreach (var offset in spray) {
-                world.Spawn (new Bullet (p + offset, v, Color.Red));
+                world.Spawn (new VerticalBounceBullet (p + offset, v, Color.Red));
             }
         }
 
